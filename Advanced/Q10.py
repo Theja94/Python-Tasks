c26c89d8f5c40b89ebcb44fda52699e845bbe859
Theja94~ <thejus.sjn94@gmail.com>
2023-08-08 19:34:23 -0400

    updated with demo.txt

--- Advanced/Q10.py
+++ Advanced/Q10.py
@@ -3,11 +3,6 @@ use these computers. A customer will be serviced only if there is
 any unoccupied computer at the moment the customer visits the
 cafe. If there is no unoccupied computer, the customer leaves the
 cafe.
-
-training.consultadd.com
-
-LEVEL - II
-
 You are given an integer N representing the number of computers
 in the cafe and a sequence of uppercase letters S. Every letter in S
 occurs exactly two times. The first occurrence denotes the arrival
@@ -33,3 +28,33 @@ Output: 2
 Explanation: B and C will not be able to get any computers. So the
 answer is 2.
 """
+MAX_CHAR = 26
+
+
+def cust_details(n, s):
+
+	seen = [0] * MAX_CHAR
+
+	res = 0
+	occupied = 0 
+
+	for i in range(len(s)):
+
+		ind = ord(s[i]) - ord('A')
+		if seen[ind] == 0:
+			seen[ind] = 1
+			if occupied < n:
+				occupied+=1
+				seen[ind] = 2
+			else:
+				res+=1
+		else:
+			if seen[ind] == 2:
+				occupied-=1
+			seen[ind] = 0
+
+	return res
+
+print (cust_details(3, "GACCBDDBAGEE"))
+print (cust_details(1, "ABCBAC"))
+
